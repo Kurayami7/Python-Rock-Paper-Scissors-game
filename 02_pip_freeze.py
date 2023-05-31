244d049de24138cbd5a3d047005d4045c7e4ecba
Darkuser <124408792+Kurayami7@users.noreply.github.com>
2023-05-31 19:07:34 -0400

    Add files via upload

---
+++ 02_pip_freeze.py
@@ -0,0 +1,14 @@
+# Pip Freeze Command 
+'''Pip freeze returns all the packages installed in a given python
+environment along with the versions.
+pip freeze > requirements.txt
+The command above creates a file named 'requirements.txt' in the 
+same directory containing the output of pip freeze.
+
+We can also distribute the created file to other users and they 
+can recreate the same environment using:
+pip install -r requirements.txt
+'''
+
+
+
