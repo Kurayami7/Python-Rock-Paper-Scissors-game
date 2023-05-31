5dfd76699b7f8c5c50d481fe09579c217b80b420
Darkuser <124408792+Kurayami7@users.noreply.github.com>
2023-05-31 19:07:05 -0400

    Add files via upload

---
+++ 01_virtual_environment.py
@@ -0,0 +1,19 @@
+# Virtual Environment 
+'''An environment which is the same as the system interpretor
+but is isolated from the other python environments on the 
+system.
+
+# To install, create & activate a virtual env:
+pip install virtualenv in terminal.
+A new project can be created using virtualenv (name).
+To activate the Virtualenv in the terminal, use 
+.\myprojectenv\Scripts\activate.ps1
+
+Note: Run 'Set-ExecutionPolicy -Scope CurrentUser' in powershell
+in case of an execution policy error
+'''
+
+'''Note: It is important to create a new terminal when 
+switching interpretors to utilize them in the terminal.
+It is possible to deactivate terminals by typing 'deactivate'
+in the terminal'''
